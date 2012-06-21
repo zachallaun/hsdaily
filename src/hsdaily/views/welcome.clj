6d84b8251c03b672b9d561577e9a72b76e116995
Zachary Allaun <zach.allaun@gmail.com>
2012-06-21 11:39:22 -0400

    And so it was

---
+++ src/hsdaily/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns hsdaily.views.welcome
+  (:require [hsdaily.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to hsdaily"]))
