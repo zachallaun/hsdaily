8e0e1f1a57c029929f31ed554535fd827948e32c
Zachary Allaun <zach.allaun@gmail.com>
2012-06-26 18:23:00 -0400

    Unify add! and update! into insert

--- src/hsdaily/models/user.clj
+++ src/hsdaily/models/user.clj
@@ -9,22 +9,14 @@
 (def username->id (partial hsq/q-first-id-with-key @conn :user/username))
 
 (defn prep-user [{:keys [login email auth-token avatar_url] :as user}]
-  {:user/username login
+  {:db/id (d/tempid :db.part/user)
+   :user/username login
    :user/email email
    :user/auth-token auth-token
    :user/avatar-url (java.net.URI. avatar_url)})
 
-(defn prep-new [user]
-  (assoc (prep-new user) :db/id (d/tempid :db.part/user)))
-
-(defn add! [user]
-  @(d/transact @conn [(prep-new user)]))
-
-(defn update! [user]
-  (let [entity (d/entity (db @conn) (username->id (:login user)))
-        entity (into {:db/id (:db/id entity)} entity)
-        updates (prep-user user)]
-    @(d/transact @conn [(merge entity updates)])))
+(defn insert! [user]
+  @(d/transact @conn [(prep-user user)]))
 
 (defn current-user []
   (when-let [token (session/get :auth-token)]
@@ -38,7 +30,8 @@
   [temp-code]
   (let [token (gh/get-token temp-code)
         user (gh/get-user token)]
-    (if (nil? (username->id (:login user)))
-      (add! (assoc user :auth-token token))
-      (update! (assoc user :auth-token token)))
+    ;; (if (nil? (username->id (:login user)))
+    ;;   (add! (assoc user :auth-token token))
+    ;;   (update! (assoc user :auth-token token)))
+    (insert! (assoc user :auth-token token))
     (d/entity (db @conn) (username->id (:login user)))))
