0fb6e33f4ff0bc06d515a33382f30393032a0f26
Zachary Allaun <zach.allaun@gmail.com>
2012-06-29 14:35:14 -0400

    Model projects as repos and commits instead

--- test/hsdaily/test/models.clj
+++ test/hsdaily/test/models.clj
@@ -3,36 +3,15 @@
         [noir.util.test :only [with-noir]]
         [datomic.api :only [q db] :as d]
         [hsdaily.db :only [conn] :as hsdb])
-  (:require [hsdaily.models.proj :as projs]
+  (:require [hsdaily.models.repo :as repos]
             [hsdaily.models.user :as users]
             [hsdaily.query :as hsq]))
 
-(deftest project-model
+(deftest repo-model
 
   (testing "project model validations"
-    (is (true? (with-noir (projs/valid? {:name "Test Project"}))))
-    (is (false? (with-noir (projs/valid? {})))))
 
-  (testing "project document preparation"
-    (is (contains? (projs/prep-new {})
-                   :project/desc))
-    (is (= (:project/desc (projs/prep-new {:desc "test description"}))
-           "test description")))
-
-  (testing "project document insertion"
-
-    (testing "single insert"
-      (with-noir (projs/add! {:name "test proj 1"}))
-
-      (is (= (count (q hsq/all-with-attr (db @conn) :project/name))
-             1)))
-
-    (testing "batch insert"
-      (with-noir (projs/add! [{:name "test proj 2"}
-                              {:name "test proj 3"}]))
-
-      (is (= (count (q hsq/all-with-attr (db @conn) :project/name))
-             3)))))
+    (is (= true false) "Add repo tests")))
 
 (deftest user-model
 
@@ -43,5 +22,5 @@
 (defn test-ns-hook []
   ;; Connect to clean memory db
   (hsdb/retract-all conn hsdb/mem-uri)
-  (project-model)
+  (repo-model)
   (user-model))
